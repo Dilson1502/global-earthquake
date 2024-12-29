aa8e041ad00c32d2988d37a50e9f5f86f346bae3
Dilson Castro Hernández <castro.do@pg.com>
2024-12-28 21:47:49 -0500

    Add support for html

--- src/api/main.py
+++ src/api/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Dict
 from datetime import datetime
 from src.transformations.earthquake import top_n_nearest_earthquakes
 
 app = FastAPI()
+
+# Allow cross-origin requests (CORS)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins, you can restrict this in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -19,8 +32,7 @@ async def read_root():
             <h1>Welcome to the Earthquake API!</h1>
             <p>Creator: Dilson Castro</p>
             <p>Current Date and Time: {current_datetime}</p>
-            <p>To find earthquakes near you, please navigate to <a href="http://localhost:5005/docs/">This Link</a>.
-            </p>
+            <p>To find earthquakes near you, please navigate to <a href="/static/index.html">This Link</a>.</p>
         </body>
     </html>
     """
@@ -29,10 +41,12 @@ async def read_root():
 
 @app.get("/earthquakes/near-me")
 async def nearest_earthquakes(lat: float, lon: float) -> Dict:
-    """Get top n earthquakes near to an entered location
+    """Get top n earthquakes near to an entered location.
+
     Args:
-        lat (float): input latitud.
+        lat (float): input latitude.
         lon (float): input longitude.
+
     Returns:
         Dict: nearest earthquakes in json format.
     """
